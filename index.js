e3822c7f948d0d52abc08b788b681269136438c7
Shabinn-k <shhabin.07@gmail.com>
2026-01-28 12:53:06 +0530

    learning from scratch

---
+++ index.js
@@ -0,0 +1,6 @@
+let UserName = "shabin";
+let Phone = 9995977246;
+let isLogged = true;
+let empty = [];
+let skills = ["html", "css", "js"];
+let even = [2, 4, 6, 8, 10];
---
+++ index.ts
@@ -0,0 +1,6 @@
+let UserName:string="shabin"
+let Phone:number=9995977246
+let isLogged:boolean=true
+let empty:[]=[]
+let skills:string[]=["html","css","js"]
+let even:number[]=[2,4,6,8,10]
