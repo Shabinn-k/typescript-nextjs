fdf875c3ec5c232a097f08cd5d101afba885b9ae
Shabinn-k <shhabin.07@gmail.com>
2026-01-30 10:18:15 +0530

    learned interface and doing task with obj

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ let isLogged = true;
 let empty = [];
 let skills = ["html", "css", "js"];
 let even = [2, 4, 6, 8, 10];
+let UserDetails = {
+    name: "shabin",
+    age: 18,
+    isPresent: true
+};
+let AdminDetails = {
+    name: "admin",
+    age: 20,
+    isPresent: false
+};
+let mix = ["a", "b", "c", 1];
--- index.ts
+++ index.ts
@@ -1,6 +1,29 @@
-let UserName:string="shabin"
-let Phone:number=9995977246
-let isLogged:boolean=true
-let empty:[]=[]
-let skills:string[]=["html","css","js"]
-let even:number[]=[2,4,6,8,10]
+let UserName: string = "shabin"
+let Phone: number = 9995977246
+let isLogged: boolean = true
+let empty: [] = []
+let skills: string[] = ["html", "css", "js"]
+let even: number[] = [2, 4, 6, 8, 10]
+// let UserDetails:{name:string,age:number}={
+//     name:"shabin",
+//     age:18
+// }
+
+interface Details {
+    name: string,
+    age: number,
+    isPresent?: boolean
+}
+
+let UserDetails: Details = {
+    name: "shabin",
+    age: 18,
+    isPresent: true
+}
+let AdminDetails: Details = {
+    name: "admin",
+    age: 20,
+    isPresent: false
+}
+
+let mix: (string | number)[] = ["a", "b", "c", 1]
